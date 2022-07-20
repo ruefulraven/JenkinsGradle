f89cae1848f7a8d1ba5cf42eb6bc294e543698da
Axel <axelbajan66@gmail.com>
2022-07-20 13:33:18 +0800

    Adding JUnit Tests

--- src/test/java/com/gradle/cwt/JenkinsGradleProjApplicationTests.java
+++ src/test/java/com/gradle/cwt/JenkinsGradleProjApplicationTests.java
@@ -1,13 +1,31 @@
 package com.gradle.cwt;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import com.gradle.cwt.entity.Product;
+import com.gradle.cwt.service.ProductService;
+import com.gradle.cwt.service.ProductServiceImpl;
 
 @SpringBootTest
 class JenkinsGradleProjApplicationTests {
-
+	@Autowired
+	private ProductService service;
+	
+	
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void returnProject() {
+		List<Product> procutList = service.getAllProducts();
+		Assert.isNull(procutList, "Object must be null");
 	}
 
 }
