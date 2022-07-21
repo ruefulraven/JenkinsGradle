dcc466a14d8c1c84554bfcec1269e335303f8641
Axel <axelbajan66@gmail.com>
2022-07-21 14:54:49 +0800

    updated first Task

--- src/test/java/com/gradle/cwt/JenkinsGradleProjApplicationTests.java
+++ src/test/java/com/gradle/cwt/JenkinsGradleProjApplicationTests.java
@@ -25,7 +25,7 @@ class JenkinsGradleProjApplicationTests {
 	@Test
 	public void returnProject() {
 		List<Product> procutList = service.getAllProducts();
-		//procutList = null;
+		procutList = null;
 		Assert.isNull(procutList, "Object must be null");
 		//procutList = service.getAllProducts();
 		Assert.isTrue(procutList.size() > 0, "Size must be less than 0");
